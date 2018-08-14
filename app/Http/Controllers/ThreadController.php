cac126813003f77bd88965894961be8e3e6178ca
vagrant <vagrant@homestead>
2018-08-14 07:16:00 +0000

    15 A Lesson in Refactoring

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Channel;
+use App\Filters\ThreadsFilters;
 use App\Thread;
 use Illuminate\Http\Request;
 
@@ -18,21 +19,9 @@ class ThreadController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Channel $channel)
+    public function index(Channel $channel,ThreadsFilters $filters)
     {
-        if($channel->exists){
-            $threads = $channel->threads()->latest();
-        }else{
-            $threads = Thread::latest();
-        }
-
-        if($username = request('by')){
-            $user = \App\User::where('name',$username)->firstOrFail();
-
-            $threads->where('user_id',$user->id);
-        }
-
-        $threads  = $threads->get();
+        $threads = $this->getThreads($channel, $filters);
 
         return view('threads.index',compact('threads'));
     }
@@ -114,5 +103,17 @@ class ThreadController extends Controller
     public function destroy(Thread $thread)
     {
         //
+    }
+
+    protected function getThreads(Channel $channel, ThreadsFilters $filters)
+    {
+        $threads = Thread::latest()->filter($filters);
+
+        if ($channel->exists) {
+            $threads->where('channel_id', $channel->id);
+        }
+
+        $threads = $threads->get();
+        return $threads;
     }
 }
