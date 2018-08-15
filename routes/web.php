35fed3fed04bc6168e9319d7fd35f9e3f5c12716
ZH117 <15319957499@163.com>
2018-08-15 03:19:00 +0000

    22 A User Has A Profile

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,16 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('threads','ThreadController@index');
-Route::get('threads/create','ThreadController@create');
-Route::get('threads/{channel}','ThreadController@index');
+
+Route::get('threads',                   'ThreadController@index');
+Route::get('threads/create',            'ThreadController@create');
+Route::get('threads/{channel}',         'ThreadController@index');
 Route::get('threads/{channel}/{thread}','ThreadController@show');
-Route::post('threads','ThreadController@store');
+Route::post('threads',                  'ThreadController@store');
+
 Route::post('/threads/{channel}/{thread}/replies','ReplyController@store');
+
 Route::post('/replies/{reply}/favorites','FavoriteController@store');
+
+Route::get('/profiles/{user}','ProfilesController@show');
+Route::get('/profiles/{user}','ProfilesController@show')->name('profile');
