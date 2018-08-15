6f0a218d168a7ea5bebde7a88e10763994907726
ZH117 <15319957499@163.com>
2018-08-15 09:30:59 +0000

    27 Extracting a Controller Query to the Model

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Activity;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -11,7 +12,7 @@ class ProfilesController extends Controller
     {
         return view('profiles.show',[
             'profileUser'=> $user,
-            'activities' => $this->getActivity($user)
+            'activities' => Activity::feed($user)
         ]);
     }
 
--- tests/Unit/ActivityTest.php
+++ tests/Unit/ActivityTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Activity;
+use Carbon\Carbon;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
@@ -37,5 +38,29 @@ class ActivityTest extends TestCase
         $reply = create('App\Reply');
 
         $this->assertEquals(2,Activity::count());
+    }
+
+    /** @test */
+    public function it_fetches_a_feed_for_any_user()
+    {
+        $this->signIn();
+
+        // Given we have a thread
+        // And another thread from a week ago
+        create('App\Thread',['user_id' => auth()->id()],2);
+
+        auth()->user()->activity()->first()->update(['created_at' => Carbon::now()->subWeek()]);
+
+        // When we fetch their feed
+        $feed = Activity::feed(auth()->user());
+
+        // Then,it should be returned in the proper format.
+        $this->assertTrue($feed->keys()->contains(
+            Carbon::now()->format('Y-m-d')
+        ));
+
+        $this->assertTrue($feed->keys()->contains(
+            Carbon::now()->subWeek()->format('Y-m-d')
+        ));
     }
 }
