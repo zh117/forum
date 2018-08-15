b34da6736429c15f50d93fccc9846c730670d44a
ZH117 <15319957499@163.com>
2018-08-15 08:41:47 +0000

    26 How to Construct an Activity Feeds Part 2

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -10,8 +10,21 @@ class ProfilesController extends Controller
     public function show(User $user)
     {
         return view('profiles.show',[
-            'profileUser'   => $user,
-            'threads'       => $user->threads()->paginate(10)
+            'profileUser'=> $user,
+            'activities' => $this->getActivity($user)
         ]);
+    }
+
+    /**
+     * @param User $user
+     * @return static
+     */
+    protected function getActivity(User $user)
+    {
+        $activities = $user->activity()->latest()->with('subject')->take(50)->get()->groupBy(function ($activity) {
+            return $activity->created_at->format('Y-m-d');
+        });
+
+        return $activities;
     }
 }
