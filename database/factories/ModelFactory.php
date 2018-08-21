ee99d3e24c767c71e31dd1ee92246f30dff4ebd0
ZH117 <15319957499@163.com>
2018-08-21 06:29:21 +0000

    71 Users Must Confirm Their Email Address: #1 - Protection

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,6 +22,7 @@ $factory->define(App\User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('123456'),
         'remember_token' => str_random(10),
+        'confirmed' => false,
     ];
 });
 
--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('threads',                       'ThreadController@index');
 Route::get('threads/create',                'ThreadController@create');
 Route::get('threads/{channel}',             'ThreadController@index');
 Route::get('threads/{channel}/{thread}',    'ThreadController@show');
-Route::post('threads',                      'ThreadController@store');
+Route::post('threads',                      'ThreadController@store')->middleware('must-be-confirmed');;
 Route::delete('threads/{channel}/{thread}', 'ThreadController@destroy');
 
 Route::get('/threads/{channel}/{thread}/replies',   'ReplyController@index');
